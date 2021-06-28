094e9a0b40fe7d88a4529f78ad307e5afeda6d92
sjw03115 <sjw03115@gmail.com>
2021-06-28 17:51:32 +0900

    ChiwppoGame
    Game씬 아트웍 올리기/리지드바디/ 근데 왜 2디스프라이트 리지드바디안먹힘/

---
+++ Assets/2. Script/PlayerCtrl.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour
+{
+
+    public float speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+
+        {
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+
+        {
+
+            transform.position += Vector3.right * speed * Time.deltaTime;
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.UpArrow))
+
+        {
+
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+
+        }
+
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+
+        {
+
+            transform.position += Vector3.back * speed * Time.deltaTime;
+
+        }
+    }
+}
